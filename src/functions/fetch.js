ba3df2f0760d09ff466e84205451832806307eab
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2023-02-12 11:12:40 +0100

    scrape result

--- src/functions/fetch.js
+++ src/functions/fetch.js
@@ -5,13 +5,12 @@ import axios from 'axios';
 const URL = 'https://uk49sresultstoday.co.za/';
 const object = [];
 async function scrapeData() {
-    axios.get(URL)
+    await axios.get(URL)
   .then(response => {
     const $ = cheerio.load(response.data);
     const title = $('.elementor-col-12').text();
     const re = title.replace(/(\r\n|\n|\r)/gm, "").split(' ').slice(0, 7);
     object.push(re);
-    
   })
   .catch(console.error);
   return object;
